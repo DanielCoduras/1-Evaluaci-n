6e5eb23dcf5472b3516bb99a7d9dfd1d2c1a69bf
DanielCoduras <43232540+DanielCoduras@users.noreply.github.com>
2018-11-26 08:26:06 +0000

    Delete piramide.py

--- piramide.py
+++
@@ -1,9 +0,0 @@
-def piramide():
-    y = input  ("Indica de que altura deseas tu piramide: ")
-    for i in range (0,y):
-        for i in range (0,i+1,1):
-            print "*",
-        print ""
-        
-        
-piramide()
