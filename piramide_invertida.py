6a5d9146a77eababa72b5973be18c5ed0459f9ca
DanielCoduras <43232540+DanielCoduras@users.noreply.github.com>
2018-11-26 08:26:14 +0000

    Delete piramide_invertida.py

--- piramide_invertida.py
+++
@@ -1,9 +0,0 @@
-def piramide_invertida():
-    y = input ("Dame y: ")
-    for i in range (y,0):
-        for i in range (0,i+1,1):
-            print "*",
-        print ""
-
-
-piramide_invertida()
