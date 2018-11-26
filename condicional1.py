23652665e83b7566ddd27b3a4f092ecb29905be1
DanielCoduras <43232540+DanielCoduras@users.noreply.github.com>
2018-11-26 08:24:14 +0000

    Delete condicional1.py

--- condicional1.py
+++
@@ -1,8 +0,0 @@
-def condicional_1():
-    nombre=raw_input("Como te llamas: ")
-    respuesta=raw_input(nombre + " vienes o te vas?")
-    if(respuesta == "voy"):
-        print "Hola "+ nombre
-    else:
-        print "Adios "+ nombre
-condicional_1()
